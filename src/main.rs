fa72c320cdbbd3a383ab3b0bd78532b7f3f72869
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2020-11-03 22:06:28 +0100

    feature: Reading csv from file
    Created function for reading CSV file and printing all records
    to output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use csv::Reader;
+use std::error::Error;
+
+fn read_file(filename: &str) -> Result <(), Box<dyn Error>> {
+    let mut reader = Reader::from_path(filename)?;
+    for result in reader.records() {
+        let line = result?;
+        println!("{:?}", line);
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = String::from("population_by_country_2020.csv");
+    read_file(&filename)?;
+    Ok(())
 }
