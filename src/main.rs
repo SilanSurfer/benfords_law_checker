b28255e5f4f295bc8d41b99665998f24873457c2
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2020-11-04 23:03:10 +0100

    feat: Logging using crate log
    Replaced println with proper logging with log crate.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use csv::Reader;
 use std::error::Error;
+use log::{info, trace};
 
 fn read_file(filename: &str) -> Result <(), Box<dyn Error>> {
+    info!("Reading from file {}", filename);
     let mut reader = Reader::from_path(filename)?;
     for result in reader.records() {
         let line = result?;
-        println!("{:?}", line);
+        trace!("{:?}", line);
     }
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize logger
+    env_logger::init();
+
     let filename = String::from("population_by_country_2020.csv");
     read_file(&filename)?;
     Ok(())
