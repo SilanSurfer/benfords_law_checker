44ce6760abda0d309362064ab4a4990d1b78e9a7
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2020-11-08 16:00:04 +0100

    feat: Implementation of frequency calculation
    Added std::collections::HashMap for measuring occurences.
    Added filtering to ignore chars that are not digit.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 use csv::Reader;
+use log::{error, info, trace, warn};
+use std::collections::HashMap;
 use std::error::Error;
-use log::{info, trace, warn, error};
 
-fn read_file(filename: &str) -> Result <(), Box<dyn Error>> {
+fn read_file(filename: &str) -> Result<(), Box<dyn Error>> {
     info!("Reading from file {}", filename);
+    let mut digit_freq_map = HashMap::new();
     let mut reader = Reader::from_path(filename)?;
     for result in reader.records() {
         match result {
             Ok(record) => {
                 if let Some(population) = record.get(1) {
                     trace!("Val: {}", population);
+                    population
+                        .chars()
+                        .next()
+                        .filter(char::is_ascii_digit)
+                        .and_then(|x| {
+                            let freq = digit_freq_map.entry(x).or_insert(1);
+                            *freq += 1;
+                            trace!("{} == {:?}", x, *freq);
+                            Some(*freq)
+                        });
                 } else {
                     warn!("No population value!");
                 }
             }
             Err(err) => {
                 error!("Error while reading record!");
-                return Err(Box::new(err))
+                return Err(Box::new(err));
             }
         }
     }
