b25a4fa75d2ddff0ce13dc0bcce04904d235f23a
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2020-11-14 22:13:27 +0100

    refactor: Extracted functions to lib.rs
    Extracted all functions to lib.rs where they will be unit tested
    in the future.
    Functions get_freq_map and read_file are public, rest of the
    functions are private.

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+use csv::Reader;
+use log::{error, info, trace};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+
+pub fn read_file(filename: &str) -> Result<Reader<File>, Box<dyn Error>> {
+    info!("Reading from file {}", filename);
+    let reader = Reader::from_path(filename)?;
+    Ok(reader)
+}
+
+pub fn get_freq_map(reader: &mut Reader<File>) -> Result<HashMap<char, u64>, Box<dyn Error>> {
+    let mut digit_freq_map = HashMap::new();
+    for result in reader.records() {
+        match result {
+            Ok(record) => {
+                get_first_digit_from(&record).map(|x| update_freq_in_map(x, &mut digit_freq_map));
+            }
+            Err(err) => {
+                error!("Error while reading record!");
+                return Err(Box::new(err));
+            }
+        }
+    }
+    Ok(digit_freq_map)
+}
+
+fn update_freq_in_map(digit: char, hash_map: &mut HashMap<char, u64>) -> u64 {
+    let freq = hash_map.entry(digit).or_insert(1);
+    *freq += 1;
+    trace!("{} == {:?}", digit, *freq);
+    *freq
+}
+
+fn get_first_digit_from(record: &csv::StringRecord) -> Option<char> {
+    match record.get(1) {
+        Some(val) => {
+            trace!("Parsing value: {}", val);
+            val.chars()
+                .next()
+                .filter(|c| c.is_ascii_digit() && *c != '0')
+        }
+        None => None,
+    }
+}
