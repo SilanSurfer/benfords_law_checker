600074ddd7a845a797eac3c54a0d896e713f3a53
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2020-11-22 23:11:04 +0100

    feat: Added configuration of logger
    Change default logger configuration:
    - Different log format:
        - only time in timestamp,
        - log level,
        - log.
    - When error log and level are in red, and level is also bold.
    - When warning then both level and log are in yellow.
    - When info then level is in green.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,46 @@ use std::error::Error;
 
 use benfords_law_checker::{display_digits_frequencies, get_occurence_map, read_file};
 
+fn configure_logger() {
+    use chrono::Local;
+    use env_logger::fmt::Color;
+    use env_logger::Builder;
+    use log::LevelFilter;
+    use std::io::Write;
+
+    let mut builder = Builder::from_default_env();
+    builder
+        .format(|buf, record| {
+            let mut level_style = buf.style();
+            let mut args_style = buf.style();
+            match record.level() {
+                log::Level::Error => {
+                    level_style.set_color(Color::Red).set_bold(true);
+                    args_style.set_color(Color::Red);
+                }
+                log::Level::Warn => {
+                    level_style.set_color(Color::Yellow);
+                    args_style.set_color(Color::Red);
+                }
+                log::Level::Info => {
+                    level_style.set_color(Color::Green);
+                }
+                _ => {}
+            }
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                Local::now().format("%H:%M:%S"),
+                level_style.value(record.level()),
+                args_style.value(record.args())
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    // Initialize logger
-    env_logger::init();
+    configure_logger();
 
     let filename = String::from("population_by_country_2020.csv");
     let mut reader = read_file(&filename)?;
