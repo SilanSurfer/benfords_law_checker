30f4bbcc4d51a284ecbba857361f3e25d7b70098
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2021-03-17 19:47:07 +0100

    feat: Created integration tests fixture with first test

---
+++ tests/cli.rs
@@ -0,0 +1,13 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("benfords_law_checker")?;
+
+    cmd.arg("test/file/doesn't/exist");
+    cmd.assert().failure().stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
