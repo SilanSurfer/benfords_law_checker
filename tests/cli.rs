e637112c51cc32f5ec8021e9a72cf9fc511c9b6e
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2021-03-18 18:22:35 +0100

    feat: Added more integration tests
    Added tests to cover following cases:
    - file is empty,
    - invalid CSV file,
    - no header, that was provided as argument, in input file.

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,7 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
+use tempfile::NamedTempFile;
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,6 +11,51 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn file_is_empty() -> Result<(), Box<dyn std::error::Error>> {
+    let file = NamedTempFile::new()?;
+    let mut cmd = Command::cargo_bin("benfords_law_checker")?;
+
+    cmd.arg(file.path());
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("File doesn't contain data"));
+
+    Ok(())
+}
+
+#[test]
+fn invalid_csv_file() -> Result<(), Box<dyn std::error::Error>> {
+    use std::io::Write;
+
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "header1,header2\nfoo,bar\nquux,baz,foobar")?;
+    let mut cmd = Command::cargo_bin("benfords_law_checker")?;
+
+    cmd.arg(file.path());
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("CSV Error"));
+
+    Ok(())
+}
+
+#[test]
+fn no_required_header() -> Result<(), Box<dyn std::error::Error>> {
+    use std::io::Write;
+
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "header1,header2\nfoo,bar\nquux,baz")?;
+    let mut cmd = Command::cargo_bin("benfords_law_checker")?;
+
+    cmd.arg(file.path()).arg("-i header3");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("There is no header"));
 
     Ok(())
 }
